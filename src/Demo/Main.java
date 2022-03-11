4021b4a0010123a1ef1f570a067e016c5c898d41
benj302g <benj302g@stud.kea.dk>
2022-03-11 09:53:52 +0100

    First commit

--- src/Demo/Main.java
+++ src/Demo/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("GIT er sejt");
+        System.out.println("fuck yeah buddy this shit rooocks");
     }
 }
